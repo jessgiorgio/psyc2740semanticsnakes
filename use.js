f99eaaa64c434585840c2ba580a1953315ca4b3d
jessgiorgio <144818599+jessgiorgio@users.noreply.github.com>
2023-09-26 21:59:05 -0400

    use and use_procedure for "use novel word in sentence"

---
+++ use.js
@@ -0,0 +1,15 @@
+var use_cues =[
+    {
+        cue: "Write a sentence using the word foobly."
+    },
+    {
+        cue: "Write a sentene using the word mipp."
+    },
+    {
+        cue: "Write a sentence using the word dodish."
+    },
+    {
+        cue: "Write a sentence using the word geck."
+    }
+
+]
