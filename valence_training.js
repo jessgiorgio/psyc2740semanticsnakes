9ead0b062a6f1e03c735432ff24711278a399441
jferry34 <144818537+jferry34@users.noreply.github.com>
2023-09-27 11:04:56 -0400

    Trying to load stimuli for IAT

---
+++ valence_training.js
@@ -0,0 +1,16 @@
+var valence_cues = [
+
+    {cue: "sweet"},
+    {cue: "friendly"},
+    {cue: "interesting"},
+    {cue: "honest"},
+    {cue: "funny"},
+    {cue: "selfish"},
+    {cue: "cruel"},
+    {cue: "boring"},
+    {cue: "mean"},
+    {cue: "uncofortable"},
+
+
+
+]
