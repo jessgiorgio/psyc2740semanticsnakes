4305cdeb31d4f2b61474d3f3507174e9f942dc5d
jferry34 <144818537+jferry34@users.noreply.github.com>
2023-10-12 11:02:17 -0400

    trying to fix IAT

--- novel.js
+++ novel.js
@@ -1,11 +1,25 @@
 var novel_cues = [
-{"cue": "foobly"},
-    correct key: left
+{"cue": "foobly",
+"correct_key": "L"},
 
-{"cue": "mipp"},
+{"cue": "mipp",
+"correct_key": "L"},
 
-{"cue": "dodish"},
+{"cue": "dodish",
+"correct_key": "L"},
 
-{"cue": "geck"}
+{"cue": "geck",
+"correct_key": "L"},
 
+{"cue": "hepp",
+"correct_key": "R"},
+
+{"cue": "pimm",
+"correct_key": "R"},
+
+{"cue": "kiff",
+"correct_key": "R"},
+
+{"cue": "wibb",
+"correct_key": "R"}
 ]
