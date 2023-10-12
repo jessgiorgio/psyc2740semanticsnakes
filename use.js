b766d3c21f4611f0d62c1e0fa0f6be04fb3afe6d
jessgiorgio <144818599+jessgiorgio@users.noreply.github.com>
2023-10-12 11:01:53 -0400

    correct key for valence procedure
    - added correct key to js file
    - added correct key data to valence training

--- use.js
+++ use.js
@@ -1,15 +1,15 @@
 var use_cues =[
     {
-        cue: "Write a sentence using the word foobly."
+        cue: "Write a sentence using the word <br><b>foobly</b>"
     },
     {
-        cue: "Write a sentene using the word mipp."
+        cue: "Write a sentene using the word <br><b>mipp</b>"
     },
     {
-        cue: "Write a sentence using the word dodish."
+        cue: "Write a sentence using the word <br><b>dodish</b>"
     },
     {
-        cue: "Write a sentence using the word geck."
+        cue: "Write a sentence using the word <br><b>geck</b>"
     }
 
 ]
--- valence_training.js
+++ valence_training.js
@@ -1,14 +1,44 @@
 var valence_cues = [
-    {"cue": "sweet"},
-    {"cue": "friendly"},
-    {"cue": "interesting"},
-    {"cue": "honest"},
-    {"cue": "funny"},
-    {"cue": "selfish"},
-    {"cue": "cruel"},
-    {"cue": "boring"},
-    {"cue": "mean"},
-    {"cue": "uncomfortable"},
+    {
+    "cue": "sweet",
+    "correct_key": "A"
+},
+    {
+    "cue": "friendly",
+    "correct_key": "A"
+},
+    {
+    "cue": "interesting",
+    "correct_key": "A"
+},
+    {
+    "cue": "honest",
+    "correct_key": "A"
+},
+    {
+    "cue": "funny",
+    "correct_key": "A"
+},
+    {
+    "cue": "selfish",
+    "correct_key": "L"
+},
+    {
+    "cue": "cruel",
+    "correct_key": "L"
+},
+    {
+    "cue": "boring",
+    "correct_key": "L"
+},
+    {
+    "cue": "mean",
+    "correct_key": "L"
+},
+    {
+    "cue": "uncomfortable",
+    "correct_key": "L"
+},
 
 
 
